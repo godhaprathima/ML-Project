79d46bec6bb46949a456d8aac0798eca2592465e
godhaprathima <godha.prathima@gmail.com>
2020-06-28 09:05:47 +0530

    committed

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from PIL import Image
+
+import mlengine
+from mlengine import load_model, load_transforms, predict
+
+MODEL_PATH = 'models/model_L0.01.pt'
+
+app = Flask(__name__)
+
+model = load_model(MODEL_PATH)
+preprocess = load_transforms()
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/upload', methods=['POST', 'GET'])
+def get_input():
+	if request.method == 'POST':
+		img_file = request.files['image']
+		image = Image.open(img_file)
+		# Sending the image to ml-engine
+		label, prob = predict(model, preprocess, image)
+		return render_template('result.html', label=label, prob=prob)
+	else:
+		return render_template('index.html')
+
+
+if __name__ == '__main__':
+ 	app.run(debug=True, port=2222) 
---
+++ mlengine.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+
+from PIL import Image
+import os
+
+def load_model(path):
+    model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=True)
+
+    in_ftrs = model.fc.in_features
+    model.fc = nn.Linear(in_ftrs, 131)
+
+    for param in model.parameters():
+        param.requires_grad=True
+
+    model.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
+    model.eval()
+
+    return model
+
+def load_transforms():
+	return transforms.Compose([
+                         transforms.Resize(256),
+                         transforms.CenterCrop(224),
+                         transforms.ToTensor(),
+                         transforms.Normalize(mean=[0.485, 0.456, 0.406], 
+                                              std=[0.229, 0.224, 0.225]),
+                         ])
+#result.jpg
+def predict(net, transform, image):
+	classes = ['Apple Braeburn', 'Apple Crimson Snow', 'Apple Golden 1', 'Apple Golden 2', 'Apple Golden 3', 'Apple Granny Smith', 'Apple Pink Lady', 'Apple Red 1', 'Apple Red 2', 'Apple Red 3', 'Apple Red Delicious', 'Apple Red Yellow 1', 'Apple Red Yellow 2', 'Apricot', 'Avocado', 'Avocado ripe', 'Banana', 'Banana Lady Finger', 'Banana Red', 'Beetroot', 'Blueberry', 'Cactus fruit', 'Cantaloupe 1', 'Cantaloupe 2', 'Carambula', 'Cauliflower', 'Cherry 1', 'Cherry 2', 'Cherry Rainier', 'Cherry Wax Black', 'Cherry Wax Red', 'Cherry Wax Yellow', 'Chestnut', 'Clementine', 'Cocos', 'Corn', 'Corn Husk', 'Cucumber Ripe', 'Cucumber Ripe 2', 'Dates', 'Eggplant', 'Fig', 'Ginger Root', 'Granadilla', 'Grape Blue', 'Grape Pink', 'Grape White', 'Grape White 2', 'Grape White 3', 'Grape White 4', 'Grapefruit Pink', 'Grapefruit White', 'Guava', 'Hazelnut', 'Huckleberry', 'Kaki', 'Kiwi', 'Kohlrabi', 'Kumquats', 'Lemon', 'Lemon Meyer', 'Limes', 'Lychee', 'Mandarine', 'Mango', 'Mango Red', 'Mangostan', 'Maracuja', 'Melon Piel de Sapo', 'Mulberry', 'Nectarine', 'Nectarine Flat', 'Nut Forest', 'Nut Pecan', 'Onion Red', 'Onion Red Peeled', 'Onion White', 'Orange', 'Papaya', 'Passion Fruit', 'Peach', 'Peach 2', 'Peach Flat', 'Pear', 'Pear 2', 'Pear Abate', 'Pear Forelle', 'Pear Kaiser', 'Pear Monster', 'Pear Red', 'Pear Stone', 'Pear Williams', 'Pepino', 'Pepper Green', 'Pepper Orange', 'Pepper Red', 'Pepper Yellow', 'Physalis', 'Physalis with Husk', 'Pineapple', 'Pineapple Mini', 'Pitahaya Red', 'Plum', 'Plum 2', 'Plum 3', 'Pomegranate', 'Pomelo Sweetie', 'Potato Red', 'Potato Red Washed', 'Potato Sweet', 'Potato White', 'Quince', 'Rambutan', 'Raspberry', 'Redcurrant', 'Salak', 'Strawberry', 'Strawberry Wedge', 'Tamarillo', 'Tangelo', 'Tomato 1', 'Tomato 2', 'Tomato 3', 'Tomato 4', 'Tomato Cherry Red', 'Tomato Heart', 'Tomato Maroon', 'Tomato Yellow', 'Tomato not Ripened', 'Walnut', 'Watermelon'] 
+	input_tensor = transform(image).unsqueeze(0) # [1,3,224,224]
+	output = F.softmax(net(input_tensor), dim=1) # probs
+
+	# convert output probabilities to predicted class
+	prob, pred = torch.max(output, 1)
+	out_label = classes[pred]
+	return out_label, prob.item() * 100
